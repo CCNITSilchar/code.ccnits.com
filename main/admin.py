a97535b5b12019e3e9f4f32d2938bac84e89986f
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-04-17 14:00:08 +0530

    Inital models - Programming Language and Codes

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from main.models import *
+
+
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+	list_display = ['name', 'lang_code', 'extension']
+	search_fields = ('name', 'lang_code', 'extension')
+
+admin.site.register(ProgrammingLanguage, ProgrammingLanguageAdmin)
+
+
+class CodeAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug', 'run_count', 'programming_language', 'public']
+	search_fields = ('name', 'slug', 'programming_language', 'code')
+
+admin.site.register(Code, CodeAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProgrammingLanguage(models.Model):
+	name = models.CharField(max_length=50)
+	lang_code = models.CharField(max_length=50)
+	extension = models.CharField(max_length=10)
+
+	def __str__(self):
+		return '%s' % self.name
+
+
+class Code(models.Model):
+	name = models.CharField(max_length=100, null=True, blank=True)
+	slug = models.CharField(max_length=8, null=True, blank=True, unique=True)
+	he_slug = models.CharField(max_length=8, null=True, blank=True, unique=True)
+	code_text = models.TextField(null=True, blank=True)
+	cloned_from = models.ForeignKey("self")
+	run_count = models.IntegerField(default=0)
+	public = models.BooleanField(default=True)
+	programming_language = models.ForeignKey(ProgrammingLanguage)
+	creation_timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, null=True, blank=True)
+	update_timestamp = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+	def __str__(self):
+		return '%s' % self.name
